25ef7ab0deb63a88f8ca24d02a2ef2bf309fd274
Alina Karchevska <79846433+AlinaKarchevska@users.noreply.github.com>
2021-03-04 22:09:01 +0200

    Create Converter.cs

---
+++ Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Converter_USD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double UsdToRub = 66.51;
+            double UsdToUah = 27.22;
+            double USD;
+
+            Console.WriteLine("Введите суму в USD");
+
+            USD = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{USD} дол в руб {USD * UsdToRub}");
+            Console.WriteLine(USD + " дол в руб " + USD * UsdToRub);
+            Console.WriteLine(USD + " дол в грн " + USD * UsdToUah);
+        }
+    }
+}
